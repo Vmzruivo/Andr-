cb966a47535224dd7e6e6c0d07db79b96eaadbcc
Vmzruivo <andresuper110@gmail.com>
2026-08-10 22:40:23 -0300

    Add Codex Vitae React entrypoint

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import CodexVitae from "../codex-vitae.jsx";
+import "./app.css";
+createRoot(document.getElementById("root")).render(<CodexVitae />);
