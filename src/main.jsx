d406003fb4a7fe94bc0b9fc661e4e66b5f29f1b6
Vmzruivo <andresuper110@gmail.com>
2026-08-23 19:12:35 -0300

    feat: load final Ritual design layer in main app

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import AdminPanel from "./AdminPanel.jsx";
 import "./app.css";
 import "./mission-layout.css";
 import "./mobile-fix.css";
+import "./ritual-force.css";
 
 class ErrorBoundary extends React.Component {
   constructor(props){super(props);this.state={error:null}}
